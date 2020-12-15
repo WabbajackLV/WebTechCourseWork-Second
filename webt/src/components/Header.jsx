d0ee10239d82784477c3205238d6154bc2bfae19
Arvis <s8_krafts_a@venta.lv>
2020-12-15 05:53:49 +0200

    Added header, and header sass/css

---
+++ webt/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header(){
+    return (<header>
+        <div className="topnav">
+            <a href="">Home</a>
+            <a href="">Menu</a>
+            <a href="">About Us</a>
+            <a href="">Login</a>
+        </div>
+    </header>);
+}
+
+export default Header;
---
+++ webt/src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Section from "./Section.jsx";
+import Section2 from "./Section2.jsx";
+
+function Main(){
+    return (<Main>
+        <Section />
+        <Section2 />
+    </Main>);
+}
+
+export default Main;
